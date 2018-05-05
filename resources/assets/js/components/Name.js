77ef2cf1faa844b94bf8ded99d7c247cbb3b0c8a
begimov <begimov@gmail.com>
2018-05-05 08:26:24 +0300

    first commit

---
+++ resources/assets/js/components/Name.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Name extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
+                            <div className="card-header">NAME</div>
+
+                            <div className="card-body">
+                                {this.props.name + '!!!'}<br /><br />
+
+                                <Welcome name='AAABC' /><br /><br />
+
+                                <Clock /><br />
+                                <Clock />
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+function Welcome(props) {
+    return <h2>WELCOME functional component {props.name}</h2>;
+}
+
+class Clock extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date(),
+            name: 'NAME123'
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+        this.handleLinkClick = this.handleLinkClick.bind(this);
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>{this.state.date.toLocaleTimeString()}</h2>
+                <p>{this.state.name}</p>
+                <p><button onClick={this.handleClick}>UPDATE</button></p>
+                <p><a href="#" onClick={(e) => this.handleLinkClick(1, e)}>CLICK</a></p>
+            </div>
+        );
+    }
+
+    handleClick() {
+        this.setState({name: 'UPDATED NAME Click'});
+    }
+
+    handleLinkClick(id, e) {
+        console.log(id);
+        e.preventDefault();
+        this.setState({name: 'UPDATED NAME Link Click'});
+    }
+
+    tick() {
+        this.setState(((prevState, props) => {
+            return {
+                date: new Date()
+            }
+        }));
+    }
+}
+
